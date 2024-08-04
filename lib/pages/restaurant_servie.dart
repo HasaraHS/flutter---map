68f1c16bd3835e5f9bd25af40bbd130a993936ee
Hasara Samarawickrama <Hasaragr1920@gmail.com>
2024-08-04 10:47:41 +0530

    commit

---
+++ lib/pages/restaurant_servie.dart
@@ -0,0 +1,26 @@
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'restaurant_model.dart';
+
+class RestaurantService {
+  final String apiKey;
+
+  RestaurantService({required this.apiKey});
+
+  Future<List<Restaurant>> fetchRestaurants(String preference, double lat, double lng) async {
+    final response = await http.get(
+      Uri.parse(
+        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$lng&radius=1500&type=restaurant&keyword=$preference&key=$apiKey',
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      final results = json['results'] as List;
+      return results.map((place) => Restaurant.fromJson(place)).toList();
+    } else {
+      throw Exception('Failed to load restaurants');
+    }
+  }
+}
