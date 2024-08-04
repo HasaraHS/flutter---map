eafb0cec589a8f4cffb720090633556cb54505d5
Hasara Samarawickrama <Hasaragr1920@gmail.com>
2024-08-04 13:20:31 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-// lib/main.dart
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:map/pages/restaurant_servie.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:async';
 import 'pages/restaurant_model.dart';
-
+import 'pages/restaurant_servie.dart'; 
 
 void main() => runApp(MyApp());
 
@@ -27,36 +26,97 @@ class MapSampleState extends State<MapSample> {
   late GoogleMapController mapController;
   final LatLng _center = const LatLng(45.521563, -122.677433);
   final List<Marker> _markers = [];
-  late RestaurantService _restaurantService;
+  late StreamSubscription<Position> positionStream;
 
   @override
   void initState() {
     super.initState();
-    _restaurantService = RestaurantService(apiKey: 'YOUR_API_KEY');
-    _fetchAndDisplayRestaurants();
+    _requestLocationPermission();
+  }
+
+  Future<void> _requestLocationPermission() async {
+    var status = await Permission.location.status;
+    if (!status.isGranted) {
+      await Permission.location.request();
+    }
+    _startPositionStream();
+  }
+
+  void _startPositionStream() {
+    positionStream = Geolocator.getPositionStream().listen(
+      (Position position) {
+        print('${position.latitude}, ${position.longitude}');
+        _updateCameraPosition(position);
+      },
+    );
+  }
+
+  void _updateCameraPosition(Position position) {
+    final newCameraPosition = CameraPosition(
+      target: LatLng(position.latitude, position.longitude),
+      zoom: 15.0,
+    );
+    mapController.animateCamera(CameraUpdate.newCameraPosition(newCameraPosition));
+  }
+
+  @override
+  void dispose() {
+    positionStream.cancel();
+    mapController.dispose();
+    super.dispose();
   }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    _fetchAndDisplayRestaurants();
   }
 
   Future<void> _fetchAndDisplayRestaurants() async {
     Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    List<Restaurant> restaurants = await _restaurantService.fetchRestaurants('pizza', position.latitude, position.longitude);
+    List<Restaurant> restaurants = await fetchRestaurants('pizza', position.latitude, position.longitude);
 
     setState(() {
       _markers.clear();
       for (final restaurant in restaurants) {
+        final markerId = MarkerId(restaurant.id);
+        double distanceInMeters = Geolocator.distanceBetween(
+          position.latitude,
+          position.longitude,
+          restaurant.lat,
+          restaurant.lng,
+        );
         _markers.add(Marker(
-          markerId: MarkerId(restaurant.id),
+          markerId: markerId,
           position: LatLng(restaurant.lat, restaurant.lng),
           infoWindow: InfoWindow(
             title: restaurant.name,
-            snippet: restaurant.rating.toString(),
+            snippet: 'Rating: ${restaurant.rating}\nDistance: ${distanceInMeters.toStringAsFixed(2)} meters',
           ),
+          onTap: () => _onMarkerTapped(markerId),
         ));
       }
     });
+  }
+
+  void _onMarkerTapped(MarkerId markerId) {
+    final marker = _markers.firstWhere((m) => m.markerId == markerId);
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                title: Text(marker.infoWindow.title ?? ''),
+                subtitle: Text(marker.infoWindow.snippet ?? ''),
+              ),
+              // Add more details here, such as reviews, contact number, etc.
+            ],
+          ),
+        );
+      },
+    );
   }
 
   @override
